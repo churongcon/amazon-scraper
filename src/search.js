4e25ff5be378bd83fd4345599e149a270f852fb7
churongcon <thuan.huynh14127@std.ttu.edu.vn>
2022-06-27 14:54:23 +0800

    Update search.js

--- src/search.js
+++ src/search.js
@@ -2,38 +2,19 @@ import fixText from "./fixtext";
 import { PDFDocument, StandardFonts, rgb } from "./pdf-lib";
 
 async function mergeAllPDFs(urls) {
-  const jpgUrl = 'https://pdf-lib.js.org/assets/cat_riding_unicorn.jpg'
-  const pngUrl = 'https://pdf-lib.js.org/assets/minions_banana_alpha.png'
-
-  const jpgImageBytes = await fetch(jpgUrl).then((res) => res.arrayBuffer())
-  const pngImageBytes = await fetch(pngUrl).then((res) => res.arrayBuffer())
-
-  const pdfDoc = await PDFDocument.create()
-
-  const jpgImage = await pdfDoc.embedJpg(jpgImageBytes)
-  const pngImage = await pdfDoc.embedPng(pngImageBytes)
-
-  const jpgDims = jpgImage.scale(0.5)
-  const pngDims = pngImage.scale(0.5)
-
-  const page = pdfDoc.addPage()
-
-  page.drawImage(jpgImage, {
-    x: page.getWidth() / 2 - jpgDims.width / 2,
-    y: page.getHeight() / 2 - jpgDims.height / 2 + 250,
-    width: jpgDims.width,
-    height: jpgDims.height,
-  })
-  page.drawImage(pngImage, {
-    x: page.getWidth() / 2 - pngDims.width / 2 + 75,
-    y: page.getHeight() / 2 - pngDims.height + 250,
-    width: pngDims.width,
-    height: pngDims.height,
-  })
-
+	const numDocs = urls.length;
+	const pdfDoc = await PDFDocument.create();
+	for(var i = 0; i < numDocs; i++) {
+		const jpgImageBytes = await fetch(urls[i]).then((res) => res.arrayBuffer())
+		const jpgImage = await pdfDoc.embedJpg(jpgImageBytes)
+		const page = pdfDoc.addPage()
+		page.drawImage(jpgImage, {
+			width: page.getWidth(),
+			height: page.getHeight(),
+		})
+	}
     const pdfDataUri = await pdfDoc.save();
     return pdfDataUri;
-  
     // strip off the first part to the first comma "data:image/png;base64,iVBORw0K..."
     // var data_pdf = pdfDataUri.substring(pdfDataUri.indexOf(',')+1);
 }
