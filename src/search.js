a675f8a8034287b2a362281928385019ab70489f
churongcon <thuan.huynh14127@std.ttu.edu.vn>
2022-06-27 11:46:30 +0800

    Update search.js

--- src/search.js
+++ src/search.js
@@ -45,7 +45,7 @@ export default async function searchProducts(query, host) {
   for (i = 1; i < all_product.length; i++) {
 	  result.push({
           name: fixText(
-            all_product[i].split('srcset="')[1].split('id="slide-image')[0].split(", ")[-1].split(' ')[1]
+            all_product[i].split('srcset="')[1].split('id="slide-image')[0]
           )
 	  }
 	 )
