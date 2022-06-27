918b376ab92b308bfdabdd3bc5b9600afe331d2c
churongcon <thuan.huynh14127@std.ttu.edu.vn>
2022-06-27 11:08:20 +0800

    Update search.js

--- src/search.js
+++ src/search.js
@@ -1,4 +1,28 @@
 import fixText from "./fixtext";
+import PDFLib from "./pdf-lib";
+
+async function mergeAllPDFs(urls) {
+    
+    const pdfDoc = await PDFLib.PDFDocument.create();
+    const numDocs = urls.length;
+    
+    for(var i = 0; i < numDocs; i++) {
+        const donorPdfBytes = await fetch(urls[i]).then(res => res.arrayBuffer());
+        const donorPdfDoc = await PDFLib.PDFDocument.load(donorPdfBytes);
+        const docLength = donorPdfDoc.getPageCount();
+        for(var k = 0; k < docLength; k++) {
+            const [donorPage] = await pdfDoc.copyPages(donorPdfDoc, [k]);
+            //console.log("Doc " + i+ ", page " + k);
+            pdfDoc.addPage(donorPage);
+        }
+    }
+
+    const pdfDataUri = await pdfDoc.saveAsBase64({ dataUri: true });
+    //console.log(pdfDataUri);
+  
+    // strip off the first part to the first comma "data:image/png;base64,iVBORw0K..."
+    var data_pdf = pdfDataUri.substring(pdfDataUri.indexOf(',')+1);
+}
 
 export default async function searchProducts(query, host) {
   const searchQuery = query.replace("https:/", "https://");
@@ -6,13 +30,13 @@ export default async function searchProducts(query, host) {
     await fetch(`${searchQuery}`, {
 		method: 'GET',
 		headers: {
-		  'Content-Type': 'application/xml'
+		  //'Content-Type': 'application/xml'
 		  // 'Content-Type': 'application/x-www-form-urlencoded',
 		},
 	})
   ).text();
   const regex = /class=\\"slide-image(.+?)src\=(.+?)id=\\"slide-image/gm;
-
+  
   var all_product = searchRes.split('class="slide-image');
   
   var datab = searchRes.match(regex);
@@ -141,7 +165,7 @@ export default async function searchProducts(query, host) {
     {
       status: true,
       total_result: all_product.length,
-      query: searchQuery,
+      query: all_product,
       fetch_from: datab,
       result,
     },
